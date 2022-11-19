be9adafa716292fd4335c9c727009c6853ba8f5e
zerkijulian92 <zerkijuliandri92@gmail.com>
2022-11-19 10:45:50 +0700

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore voluptatibus iusto sed, quos obcaecati adipisci dolores corporis. Suscipit, eaque. Magni consequatur, quia perspiciatis delectus explicabo labore odio facere fugit deserunt.</p>
 </body>
 </html>
