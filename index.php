2d63ed005fb4d7e59f32b349c512dd534aa31153
zerkijulian92 <zerkijuliandri92@gmail.com>
2022-11-19 10:57:53 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore voluptatibus iusto sed, quos obcaecati adipisci dolores corporis. Suscipit, eaque. Magni consequatur, quia perspiciatis delectus explicabo labore odio facere fugit deserunt.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
